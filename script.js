e45ac43631a0a35d4fa0df4d7411bedb1a540c16
KAIQUE PEREIRA DA CONCEICAO <kaique.pconceicao1@senacsp.edu.br>
2024-09-24 15:21:35 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let nome = prompt ("Digite seu nome");
+let numero1 = Number (prompt ("Digite a sua idade"));
+let anosparacem = 100 - numero1
+
+alert(`Bem vindo (a) ${nome}, você completará 100 anos em ${anosparacem} anos`);
+
+
+// Number (linha 2) = suporta números inteiros e com vírgula
+//  Também temos uma propriedade que poderíamos usar que é o "parseint" após o sinal de igual, essa propriedade aceita apenas números inteiros (números sem vírgula)
